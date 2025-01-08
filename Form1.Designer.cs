06f0f3ce367a49ffe99c39be40c2633c605d5c9a
DritonRamadani <DritonRamadani@AW-NB191>
2025-01-08 12:20:23 +0100

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace StrichCode_Drucker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCodeGenerate = new System.Windows.Forms.Button();
+            this.tBoxEbNummer = new System.Windows.Forms.TextBox();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCodeGenerate
+            // 
+            this.btnCodeGenerate.Location = new System.Drawing.Point(357, 172);
+            this.btnCodeGenerate.Name = "btnCodeGenerate";
+            this.btnCodeGenerate.Size = new System.Drawing.Size(112, 52);
+            this.btnCodeGenerate.TabIndex = 0;
+            this.btnCodeGenerate.Text = "Barcode Generieren";
+            this.btnCodeGenerate.UseVisualStyleBackColor = true;
+            this.btnCodeGenerate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tBoxEbNummer
+            // 
+            this.tBoxEbNummer.Location = new System.Drawing.Point(219, 91);
+            this.tBoxEbNummer.Name = "tBoxEbNummer";
+            this.tBoxEbNummer.Size = new System.Drawing.Size(250, 22);
+            this.tBoxEbNummer.TabIndex = 1;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(357, 283);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(112, 52);
+            this.btnPrint.TabIndex = 4;
+            this.btnPrint.Text = "Barcode Drucken";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Barcode",
+            "Barcode mit EB-Nummer",
+            "Barcode mit Logo und EB-Nummer"});
+            this.comboBox1.Location = new System.Drawing.Point(65, 187);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(265, 24);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "PDF erstellen",
+            "Technik Büro Drucker",
+            "Etiketten Drucker"});
+            this.comboBox2.Location = new System.Drawing.Point(65, 298);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(265, 24);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(65, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "EB-Nummer:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 687);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.tBoxEbNummer);
+            this.Controls.Add(this.btnCodeGenerate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCodeGenerate;
+        private System.Windows.Forms.TextBox tBoxEbNummer;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Diagnostics;
+using System.Windows.Forms;
+using ZXing;
+
+namespace StrichCode_Drucker
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap barcodeBitmap;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            tBoxEbNummer.Text = "EB12345678912";
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tBoxEbNummer.Text))
+            {
+                MessageBox.Show("Keine EB-Nummer eingetragen");
+            }
+
+            else if (!string.IsNullOrEmpty(tBoxEbNummer.Text) && comboBox1.SelectedIndex == 2)
+            {
+                BarcodeWriter barcodeWriter = new BarcodeWriter();
+                barcodeWriter.Format = BarcodeFormat.CODE_128;
+                barcodeWriter.Options.Height = 300;
+                barcodeWriter.Options.Width = 295;
+                barcodeWriter.Options.PureBarcode = true;
+                barcodeBitmap = barcodeWriter.Write(tBoxEbNummer.Text);
+
+                Bitmap topLogo = Properties.Resources.logo;
+                Bitmap bottomLogo = Properties.Resources.logo;
+
+                // Top Logo um 180° Drehen
+                topLogo.RotateFlip(RotateFlipType.Rotate180FlipNone);
+
+                // Kombiniertes Bild erstellen (gleiche Größe wie vorher)
+                int combinedHeight = 420;
+                int combinedWidth = 295;
+                Bitmap combinedBitmap = new Bitmap(combinedWidth, combinedHeight);
+                using (Graphics g = Graphics.FromImage(combinedBitmap))
+                {
+                    // 0. Hintergrund weiß
+                    g.Clear(Color.White);
+                    // 1. Toplogo 180°
+                    g.DrawImage(topLogo, 100, 10, 100, 50);
+                    // 2. String 180°
+                    g.TranslateTransform(150, 90); // Ursprung verschieben
+                    g.RotateTransform(180); // Text drehen
+                    using (Font font = new Font("Arial", 12, FontStyle.Bold))
+                    using (StringFormat format = new StringFormat { Alignment = StringAlignment.Center })
+                    {
+                        g.DrawString(tBoxEbNummer.Text, font, Brushes.Black, 0, 0, format);
+                    }
+                    g.ResetTransform(); // Zurücksetzen der Transformation
+                    // 3. Barcode
+                    g.DrawImage(barcodeBitmap, 0, 95, 295, 240);
+                    // 4. String
+                    using (Font font = new Font("Arial", 12, FontStyle.Bold))
+                    using (StringFormat format = new StringFormat { Alignment = StringAlignment.Center })
+                    {
+                        g.DrawString(tBoxEbNummer.Text, font, Brushes.Black, combinedWidth / 2, 340, format);
+                    }
+                    // 5. Botlogo
+                    g.DrawImage(bottomLogo, 100, 370, 100, 50);
+                }
+                barcodeBitmap = combinedBitmap;
+                MessageBox.Show("Barcode erfolgreich erstellt!");
+            }
+
+            else if (!string.IsNullOrEmpty(tBoxEbNummer.Text) && comboBox1.SelectedIndex == 1)
+            {
+                BarcodeWriter barcodeWriter = new BarcodeWriter();
+                barcodeWriter.Format = BarcodeFormat.CODE_128;
+                barcodeWriter.Options.Height = 300;
+                barcodeWriter.Options.Width = 295;
+                barcodeWriter.Options.PureBarcode = true;
+                barcodeBitmap = barcodeWriter.Write(tBoxEbNummer.Text);
+
+                // Kombiniertes Bild erstellen (gleiche Größe wie vorher)
+                int combinedHeight = 420;
+                int combinedWidth = 295;
+                Bitmap combinedBitmap = new Bitmap(combinedWidth, combinedHeight);
+                using (Graphics g = Graphics.FromImage(combinedBitmap))
+                {
+                    // 0. Hintergrund weiß
+                    g.Clear(Color.White);
+                    // 1. String 180°
+                    g.TranslateTransform(150, 35); // Ursprung verschieben
+                    g.RotateTransform(180); // Text drehen
+                    using (Font font = new Font("Arial", 12, FontStyle.Bold))
+                    using (StringFormat format = new StringFormat { Alignment = StringAlignment.Center })
+                    {
+                        g.DrawString(tBoxEbNummer.Text, font, Brushes.Black, 0, 0, format);
+                    }
+                    g.ResetTransform(); // Zurücksetzen der Transformation
+                    // 3. Barcode
+                    g.DrawImage(barcodeBitmap, 0, 45, 295, 340);
+                    // 4. String
+                    using (Font font = new Font("Arial", 12, FontStyle.Bold))
+                    using (StringFormat format = new StringFormat { Alignment = StringAlignment.Center })
+                    {
+                        g.DrawString(tBoxEbNummer.Text, font, Brushes.Black, combinedWidth / 2, 395, format);
+                    }
+                }
+                barcodeBitmap = combinedBitmap;
+                MessageBox.Show("Barcode erfolgreich erstellt!");
+            }
+
+            else if (!string.IsNullOrEmpty(tBoxEbNummer.Text) && comboBox1.SelectedIndex == 0)
+            {
+                BarcodeWriter barcodeWriter = new BarcodeWriter();
+                barcodeWriter.Format = BarcodeFormat.CODE_128;
+                barcodeWriter.Options.Height = 300;
+                barcodeWriter.Options.Width = 295;
+                barcodeWriter.Options.PureBarcode = true;
+                barcodeBitmap = barcodeWriter.Write(tBoxEbNummer.Text);
+
+                // Kombiniertes Bild erstellen (gleiche Größe wie vorher)
+                int combinedHeight = 420;
+                int combinedWidth = 295;
+                Bitmap combinedBitmap = new Bitmap(combinedWidth, combinedHeight);
+                using (Graphics g = Graphics.FromImage(combinedBitmap))
+                {
+                    // 0. Hintergrund weiß
+                    g.Clear(Color.White);
+                    // 1. Barcode
+                    g.DrawImage(barcodeBitmap, 0, 15, 295, 400);
+                }
+                barcodeBitmap = combinedBitmap;
+                MessageBox.Show("Barcode erfolgreich erstellt!");
+            }
+        }
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            if (barcodeBitmap == null)
+            {
+                MessageBox.Show("Bitte generieren Sie zuerst einen Barcode, bevor Sie drucken!", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return; // Abbrechen, wenn kein Barcode vorhanden ist
+            }
+            try
+            {
+                PrintDocument printDocument = new PrintDocument();
+
+                if (comboBox2.SelectedIndex == 0)
+                {
+                    printDocument.PrinterSettings.PrinterName = "Microsoft Print to PDF";
+                }
+
+                else if (comboBox2.SelectedIndex == 1)
+                {
+                    printDocument.PrinterSettings.PrinterName = "RICOH Aficio MP C2800";
+                }
+
+                else if (comboBox2.SelectedIndex == 2)
+                {
+                    printDocument.PrinterSettings.PrinterName = "SATO S84-ex 305dpi";
+                }
+
+                printDocument.PrintPage += (object printSender, PrintPageEventArgs printArgs) =>
+                {
+                    printArgs.Graphics.DrawImage(barcodeBitmap, -75, 10, 370, 420);
+                };
+
+                // Druck starten
+                printDocument.Print();
+            }
+
+            catch (Exception ex)
+            {
+                // Allgemeiner Fehler beim Drucken
+                MessageBox.Show($"Fehler beim Drucken: {ex.Message}", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
